f33b57ee29d11c7caf109791f114f04446384212
SuHyuk Park <park.suhyuk@gmail.com>
2020-11-22 20:37:40 +0900

    [수정] remove echo

--- init.sh
+++ init.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # tree 도구를  설치합니다
-echo sudo apt update && apt install -y tree
+sudo apt update && apt install -y tree
 
 # rc 파일을 복사합니다
-echo cp rc/.vimrc ~/
-echo cp rc/.screenrc ~/
+cp rc/.vimrc ~/
+cp rc/.screenrc ~/
 
 # docker-compose 명령어의 alias 를 등록합니다
 echo "alias d=\"docker-compose\"" >> ~/.bashrc
