35b38fa34b030518e5e6ccde01c7d477bbd6cced
psyoblade <park.suhyuk@gmail.com>
2021-07-24 23:19:25 +0900

    [수정] init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,20 @@
 #!/bin/bash
 
+course=""
+if [[ $# -ne 1 ]]; then
+    echo "./init.sh [basic|intermediate]"
+    exit 1
+fi
+
+if [[ $1 == "basic" ]] || [[ $1 == "intermediate" ]]; then
+    course=$1
+else
+    echo "./init.sh [basic|intermediate]"
+    exit 2
+fi
+
+echo "Welcome to LGDE $course Course"
+
 # tree 도구를  설치합니다
 sudo apt update && apt install -y tree
 
@@ -12,6 +27,6 @@ echo "alias d=\"docker-compose\"" >> ~/.bashrc
 echo "export EDITOR=\"vim\"" >> ~/.bashrc
 
 # course 이름 변수를 등록합니다
-echo "export course=\"intermediate\"" >> ~/.bashrc
+echo "export course=\"${course}\"" >> ~/.bashrc
 
 source ~/.bashrc
